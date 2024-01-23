42df25abd39ecfe9fa3df0ebf7ab0d7c15d863ac
Andiko Yoga <90024747+DyArdh@users.noreply.github.com>
2024-01-23 12:42:28 +0700

    feat: setup prisma, migration, seeders

---
+++ src/services/resetdbService.ts
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+import prisma from '@/libs/prismaClient';
+
+interface TableNames {
+    tableNames: string;
+    tablename: string;
+}
+
+async function main(): Promise<void> {
+    const tableNames: TableNames[] = await prisma.$queryRaw`
+        SELECT tablename 
+        FROM pg_tables 
+        WHERE schemaname='public'
+      `;
+
+    const tables = tableNames
+        .map(({ tablename }) => tablename)
+        .filter((name) => name !== '_prisma_migrations')
+        .map((name) => `"public"."${name}"`)
+        .join(', ');
+
+    await prisma.$executeRawUnsafe(`TRUNCATE TABLE ${tables} RESTART IDENTITY CASCADE`);
+}
+
+main()
+    .then(async () => {
+        console.log('Reset database success.');
+        await prisma.$disconnect();
+    })
+    .catch(async (err) => {
+        console.log(err);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
---
+++ src/services/seederService.ts
@@ -0,0 +1,54 @@
+/* eslint-disable no-console */
+import fs from 'fs';
+import path from 'path';
+
+import bcrypt from 'bcrypt';
+
+import prisma from '@/libs/prismaClient';
+
+// Read File JSON
+function getFileJson(fileName: string): any {
+    const filePath = path.join(__dirname, 'data', fileName);
+    const readFile = fs.readFileSync(filePath, 'utf-8');
+    const data = JSON.parse(readFile);
+
+    return data;
+}
+
+async function main(): Promise<void> {
+    const hashPassword = await bcrypt.hash('Suichan2203', 10);
+    const userData = {
+        name: 'Suisei Hoshimachi',
+        username: 'suichan',
+        password: hashPassword
+    };
+
+    await prisma.user.create({ data: userData });
+    await prisma.switchMount.createMany({
+        data: getFileJson('mount_data.json'),
+        skipDuplicates: true
+    });
+    await prisma.switchType.createMany({
+        data: getFileJson('type_data.json'),
+        skipDuplicates: true
+    });
+    await prisma.lubeStatus.createMany({
+        data: getFileJson('lube_data.json'),
+        skipDuplicates: true
+    });
+    await prisma.switch.createMany({
+        data: getFileJson('switch_data.json'),
+        skipDuplicates: true
+    });
+}
+
+main()
+    .then(async () => {
+        console.log('Seeding data to database successfully.');
+        await prisma.$disconnect();
+    })
+    .catch(async (err) => {
+        console.log(err);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
