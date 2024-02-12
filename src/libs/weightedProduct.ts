bef81c0daa7f83a9551cf1b9681c39895afb725f
Andiko Yoga <90024747+DyArdh@users.noreply.github.com>
2024-02-12 14:15:12 +0700

    feat: completing weighted product lib

--- src/libs/weightedProduct.ts
+++ src/libs/weightedProduct.ts
@@ -1,24 +1,17 @@
-import { weightedCriterionType } from '@/types/weightedProduct.type';
-
-const weights = {
-    bottom_out_force: { value: 7, attribute: 'benefit' },
-    actuation_travel: { value: 5, attribute: 'benefit' },
-    total_travel: { value: 5, attribute: 'benefit' },
-    lube_id: { value: 4, attribute: 'benefit' },
-    price: { value: 9, attribute: 'cost' }
-};
+import { switchType, switchAlternativeType } from '@/types/switch.type';
+import { alternativeWeightType, weightedCriterionType } from '@/types/weightedProduct.type';
 
 function criterionWeight(criterions: weightedCriterionType) {
     const totalWeight: number = Object.values(criterions).reduce((sum, weight) => sum + weight.value, 0);
     const normalizedWeights: weightedCriterionType = {} as weightedCriterionType;
 
-    for (const criterion in weights) {
+    for (const criterion in criterions) {
         const value = criterions[criterion].value / totalWeight;
         const attribute = criterions[criterion].attribute;
         const normalizedValue = attribute === 'cost' ? value * -1 : value;
 
         normalizedWeights[criterion] = {
-            value: parseFloat(normalizedValue.toFixed(3)),
+            value: normalizedValue,
             attribute: attribute
         };
     }
@@ -26,4 +19,91 @@ function criterionWeight(criterions: weightedCriterionType) {
     return normalizedWeights;
 }
 
-console.log(criterionWeight(weights));
+function alternativeCriterion(alternatives: switchType[], weights: alternativeWeightType) {
+    const result = alternatives.map((item) => {
+        item.bottom_out_force =
+            item.bottom_out_force < 50
+                ? weights.bottom_out_force.c1
+                : item.bottom_out_force < 60
+                    ? weights.bottom_out_force.c2
+                    : item.bottom_out_force < 70
+                        ? weights.bottom_out_force.c3
+                        : weights.bottom_out_force.c4;
+
+        item.actuation_travel =
+            item.actuation_travel < 2
+                ? weights.actuation_travel.c1
+                : item.actuation_travel < 2.5
+                    ? weights.actuation_travel.c2
+                    : weights.actuation_travel.c3;
+
+        item.total_travel =
+            item.total_travel < 3.5
+                ? weights.total_travel.c1
+                : item.total_travel < 4
+                    ? weights.total_travel.c2
+                    : weights.total_travel.c3;
+
+        item.lube_id = item.lube_id === 1 ? weights.lube_id.c1 : weights.lube_id.c2;
+
+        item.price =
+            item.price < 4000
+                ? weights.price.c1
+                : item.price < 5000
+                    ? weights.price.c2
+                    : item.price < 6000
+                        ? weights.price.c3
+                        : weights.price.c4;
+
+        return item;
+    });
+
+    return result;
+}
+
+function normalizedAlternative(alternatives: switchType[], weights: weightedCriterionType) {
+    return alternatives.map((alternative) => {
+        let totalS = 1;
+
+        Object.keys(weights).forEach((criterion) => {
+            const criterionValue = alternative[criterion];
+            const weight = weights[criterion].value;
+
+            if (typeof criterionValue === 'number') {
+                const normalizedValue = Math.pow(criterionValue, weight);
+                alternative[criterion] = normalizedValue;
+                totalS *= normalizedValue;
+            }
+        });
+
+        alternative.totalS = totalS;
+
+        return alternative;
+    });
+}
+
+function calculateWeightedProduct(alternatives: switchAlternativeType[]) {
+    const totalValueS: number = alternatives.reduce((sum, alternative) => sum + Number(alternative.totalS), 0);
+
+    const rankedAlternatives = alternatives.map((alternative) => ({
+        ...alternative,
+        v: (alternative.totalS ?? 0) / totalValueS
+    }));
+
+    return rankedAlternatives.sort((a, b) => b.v - a.v);
+}
+
+function weightedProduct(
+    alternative: switchType[],
+    alternativeWeights: alternativeWeightType,
+    weights: weightedCriterionType
+) {
+    const normalizedAlternatives: switchAlternativeType[] = normalizedAlternative(
+        alternativeCriterion(alternative, alternativeWeights),
+        criterionWeight(weights)
+    );
+
+    return calculateWeightedProduct(normalizedAlternatives);
+}
+
+export default weightedProduct;
--- src/types/weightedProduct.type.ts
+++ src/types/weightedProduct.type.ts
@@ -12,4 +12,33 @@ interface weightedCriterionType {
     [key: string]: weightedCriterionResult;
 }
 
-export type { weightedCriterionType };
+interface alternativeWeightType {
+    bottom_out_force: {
+        c1: number;
+        c2: number;
+        c3: number;
+        c4: number;
+    };
+    actuation_travel: {
+        c1: number;
+        c2: number;
+        c3: number;
+    };
+    total_travel: {
+        c1: number;
+        c2: number;
+        c3: number;
+    };
+    lube_id: {
+        c1: number;
+        c2: number;
+    };
+    price: {
+        c1: number;
+        c2: number;
+        c3: number;
+        c4: number;
+    };
+}
+
+export type { alternativeWeightType, weightedCriterionType };
